4ef059af11e8ab54fc824a53aa833229ce3770fe
ashurockzz <patnaashutosh@gmail.com>
2023-07-01 10:13:10 +0530

    Created Route for users and made userSchema

--- Frontend/backend/index.js
+++ Frontend/backend/index.js
@@ -5,21 +5,23 @@ const port = 5000
 const mongoDB = require('./db')
 
 mongoDB();
-// const dotenv = require("dotenv");
-// dotenv.config();
-// mongoose.connect(process.env.mongoURI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     // useFindAndModify: false,
-//     // useCreateIndex: true,   
-// })
-// .then(() => console.log("MongoDB connected!"))
-// .catch(err => console.log(err));
+const dotenv = require("dotenv");
+dotenv.config();
+mongoose.connect(process.env.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true,   
+})
+.then(() => console.log("MongoDB connected!"))
+.catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
     res.send('Hello world');
 })
+app.use(express.json())
+app.use('/api',require("./Routes/CreateUser"))
 
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`)
