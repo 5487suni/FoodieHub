56c717af2ad15e93edad8d23077286b015b556c1
ashurockzz <patnaashutosh@gmail.com>
2023-07-07 23:48:52 +0530

    done work on backend

--- Frontend/backend/db.js
+++ Frontend/backend/db.js
@@ -4,22 +4,32 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const mongoDB = async () => {
-        await mongoose.connect(process.env.mongoURI, {
+    await mongoose.connect(process.env.mongoURI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true},
-        async(err, result)=> {
+        useUnifiedTopology: true
+    },
+        async (err, result) => {
             if (err) console.log("---", err)
             else {
                 console.log("db connected");
-                const fetched_data = await mongoose.connection.db.collection('food_items');
-                fetched_data.find({}).toArray(function (err, data) {
-                    if (err) console.log(err);
-                    else {
-                        global.food_items = data
-                        // console.log(global.food_items)
-                    }
+                const fetched_data = await mongoose.connection.db.collection("food_items");
+                fetched_data.find({}).toArray(async function (err, data) {
+                    const foodCategory = await mongoose.connection.db.collection("food_category")
+                    foodCategory.find({}).toArray(function (err, catData) {
+                        if (err) console.log(err);
+
+                        else {
+                            global.food_items = data
+                            global.food_category = catData
+
+                        }
+                    })
+                    // else {
+                    //     global.food_items = data
+                    //     // console.log(global.food_items)
+                    // }
                 })
-            }   
+            }
         }
     );
 }
--- Frontend/src/screens/Home.js
+++ Frontend/src/screens/Home.js
@@ -1,18 +1,47 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Card from '../components/Card'
 import Carousel from '../components/Carousel'
 
 export default function Home() {
+
+    const [foodCat,setFoodCat] = useState([])
+    const [foodItem, setFoodItems] = useState([])
+    // const [search, setSearch] = useState('')
+
+    const loadData = async () => {
+        let response = await fetch("http://localhost:5000/api/auth/foodData", {
+          // credentials: 'include',
+          // Origin:"http://localhost:3000/login",
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+    
+        });
+        response = await response.json()
+        // console.log(response[1][0].CategoryName)
+        setFoodItems(response[0])
+        setFoodCat(response[1])
+      }
+    
+      useEffect(() => {
+        loadData()
+      }, [])
     return (
         <div>
             <div><Navbar /></div>
             <div><Carousel/></div>
-            <div className='m-3'>
-                <Card/>
-                <Card/>
-                <Card/>
+            <div className='container'>
+                {
+                    foodCat !==[]
+                    ? foodCat.map((data)=>{
+                        return (
+                            <div>Hello</div>
+                        )
+                    }):<div>""""""</div>
+                }
                 <Card/>
             </div>
             <div><Footer /> </div>
